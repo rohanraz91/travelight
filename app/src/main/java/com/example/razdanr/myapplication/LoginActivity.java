686406198efd4e5f5b1a5ba90b5e4af2b389433e
Rohan Razdan <razdanr@itserv.scss.tcd.ie>
2017-02-07 14:49:36 +0000

    Firebase added along with login authentication. (Database setup)

--- app/src/main/java/com/example/razdanr/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/razdanr/myapplication/LoginActivity.java
@@ -4,30 +4,46 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.w3c.dom.Text;
+
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-
+    private EditText etUserName;
+    private EditText etPassword;
+    private FirebaseAuth.AuthStateListener mAuthListner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         /*Variable declaration for layout items */
-        final EditText etUserName = (EditText) findViewById((R.id.etUserName));
-        final EditText etPassword = (EditText) findViewById((R.id.etPassword));
+        etUserName = (EditText) findViewById((R.id.etUserName));
+        etPassword = (EditText) findViewById((R.id.etPassword));
         final Button bLogin = (Button) findViewById((R.id.bLogin));
         //final TextView tvRegister = (TextView) findViewById((R.id.tvRegister));
         mAuth = FirebaseAuth.getInstance();
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //If user has logged In
+                if(firebaseAuth.getCurrentUser() != null){
+                    startActivity(new Intent(LoginActivity.this, UserAreaActivity.class));
+                }
+            }
+        };
         /*tvRegister.setOnClickListener(new View.OnClickListener() {
            @Override
             public void onClick(View v){
@@ -39,10 +55,34 @@ public class LoginActivity extends AppCompatActivity {
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                String email= etUserName. getText().toString();
-                String password= etPassword. getText().toString();
+                signIn();
 
             }
         });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListner);
+    }
+
+    private void signIn(){
+        String email= etUserName.getText().toString();
+        String password= etPassword.getText().toString();
+        //check if user has not left the Uname and Pass field Blanc
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "Email/ Password Field Empty", Toast.LENGTH_LONG).show();
+        }
+        else{
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "SignIn Error", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
     }
 }

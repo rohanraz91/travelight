0985518ddc427b4db8475c492d258e9e720c2b81
razdanr <rohanraz91@gmail.com>
2017-02-09 20:40:52 +0000

    Authentication of suitcaseID complete

--- app/src/main/java/com/example/razdanr/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/razdanr/myapplication/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.example.razdanr.myapplication;
 
 import android.content.Intent;
+import android.content.SyncStatusObserver;
 import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -22,13 +23,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private EditText etPassword;
     private EditText etUserName;
     private EditText etSuitcaseID;
-
+    private FirebaseDatabase database;
+    final ArrayList<String> array = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +48,27 @@ public class RegisterActivity extends AppCompatActivity {
         final TextView tvLogin = (TextView) findViewById((R.id.tvLogin));
 
         firebaseAuth= FirebaseAuth.getInstance();
+
+
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference();
+        databaseReference.child("suitcaseID").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //get all of the children at this level
+                final Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+
+                for (DataSnapshot child: children)
+                {
+                    array.add(child.getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,20 +87,30 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void registerUser(){
+        int flag=0;
         String email= etUserName.getText().toString();
         String password= etPassword.getText().toString();
-        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful())
-                {
-                    Toast.makeText(RegisterActivity.this, "Register Successfully", Toast.LENGTH_LONG).show();
-                }
-                else
-                {
-                    Toast.makeText(RegisterActivity.this, "Register Error", Toast.LENGTH_LONG).show();
-                }
+        for(int i = 0; i< array.size();i++) {
+            if (array.get(i).toString().equals(etSuitcaseID.getText().toString())) {
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Register Successfully", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Register Error", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                });
+                flag++;
+                break;
             }
-        });
+        }
+        if(flag==0)
+        {
+            Toast.makeText(RegisterActivity.this, "Register Error", Toast.LENGTH_LONG).show();
+        }
     }
 }

3bf72f01b482106681b2fd78057f1687cbda405a
razdanr <rohanraz91@gmail.com>
2017-02-23 18:50:52 +0000

    Scanning BLE device along with Listing of the devices.

--- app/src/main/java/com/example/razdanr/myapplication/UserHome.java
+++ app/src/main/java/com/example/razdanr/myapplication/UserHome.java
@@ -10,6 +10,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,14 +43,59 @@ import com.google.firebase.auth.FirebaseUser;
 public class UserHome extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private BluetoothAdapter mBluetoothAdapter;
-    private final static int REQUEST_ENABLE_BT = 1;
+    private BluetoothLeScanner mBluetoothLeScanner;
+    private boolean mScanning;
+
+    private static final int RQS_ENABLE_BLUETOOTH = 1;
+    Button bScan;
+    ListView listView;
+    List<BluetoothDevice> listBluetoothDevice;
+    ListAdapter adapterLeScanResult;
+    private Handler mHandler;
+    private static final long SCAN_PERIOD = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+
+        // Check if BLE is supported on the device.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this,
+                    "BLUETOOTH_LE not supported in this device!",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        getBluetoothAdapterAndLeScanner();
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this,
+                    "bluetoothManager.getAdapter()==null",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        bScan = (Button)findViewById(R.id.scan);
+        bScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanLeDevice(true);
+            }
+        });
+
+        listView = (ListView)findViewById(R.id.lelist);
+
+        listBluetoothDevice = new ArrayList<>();
+        adapterLeScanResult = new ArrayAdapter<BluetoothDevice>(
+                this, android.R.layout.simple_list_item_1, listBluetoothDevice);
+        listView.setAdapter(adapterLeScanResult);
+        listView.setOnItemClickListener(scanResultOnItemClickListener);
+
+        mHandler = new Handler();
+
         final Button bLogout = (Button) findViewById((R.id.bLogout));
-        final Button bScanDevices = (Button) findViewById((R.id.bScanDevices));
+
 
         final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         firebaseAuth = FirebaseAuth.getInstance();
@@ -50,18 +120,162 @@ public class UserHome extends AppCompatActivity {
             }
         });
 
-        // ScanSuitcases button tapped
-        bScanDevices.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mBluetoothAdapter = bluetoothManager.getAdapter();
-                // Ensures Bluetooth is available on the device and it is enabled. If not,
-                // displays a dialog requesting user permission to enable Bluetooth.
-                if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                }
-            }
-        });
+
     }
+    AdapterView.OnItemClickListener scanResultOnItemClickListener =
+            new AdapterView.OnItemClickListener(){
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    final BluetoothDevice device = (BluetoothDevice) parent.getItemAtPosition(position);
+
+                    String msg = device.getAddress() + "\n"
+                            + device.getBluetoothClass().toString() + "\n"
+                            + getBTDevieType(device);
+
+                    new AlertDialog.Builder(UserHome.this)
+                            .setTitle(device.getName())
+                            .setMessage(msg)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            })
+                            .show();
+
+                }
+            };
+    private String getBTDevieType(BluetoothDevice d){
+        String type = "";
+
+        switch (d.getType()){
+            case BluetoothDevice.DEVICE_TYPE_CLASSIC:
+                type = "DEVICE_TYPE_CLASSIC";
+                break;
+            case BluetoothDevice.DEVICE_TYPE_DUAL:
+                type = "DEVICE_TYPE_DUAL";
+                break;
+            case BluetoothDevice.DEVICE_TYPE_LE:
+                type = "DEVICE_TYPE_LE";
+                break;
+            case BluetoothDevice.DEVICE_TYPE_UNKNOWN:
+                type = "DEVICE_TYPE_UNKNOWN";
+                break;
+            default:
+                type = "unknown...";
+        }
+
+        return type;
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, RQS_ENABLE_BLUETOOTH);
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == RQS_ENABLE_BLUETOOTH && resultCode == Activity.RESULT_CANCELED) {
+            finish();
+            return;
+        }
+
+        getBluetoothAdapterAndLeScanner();
+
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this,
+                    "bluetoothManager.getAdapter()==null",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void getBluetoothAdapterAndLeScanner(){
+        // Get BluetoothAdapter and BluetoothLeScanner.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+
+        mScanning = false;
+    }
+
+    /*
+    to call startScan (ScanCallback callback),
+    Requires BLUETOOTH_ADMIN permission.
+    Must hold ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION permission to get results.
+     */
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            listBluetoothDevice.clear();
+            listView.invalidateViews();
+
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mBluetoothLeScanner.stopScan(scanCallback);
+                    listView.invalidateViews();
+
+                    Toast.makeText(UserHome.this,
+                            "Scan timeout",
+                            Toast.LENGTH_LONG).show();
+
+                    mScanning = false;
+                    bScan.setEnabled(true);
+                }
+            }, SCAN_PERIOD);
+
+            mBluetoothLeScanner.startScan(scanCallback);
+            mScanning = true;
+            bScan.setEnabled(false);
+        } else {
+            mBluetoothLeScanner.stopScan(scanCallback);
+            mScanning = false;
+            bScan.setEnabled(true);
+        }
+    }
+
+    private ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+
+            addBluetoothDevice(result.getDevice());
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+            for(ScanResult result : results){
+                addBluetoothDevice(result.getDevice());
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            Toast.makeText(UserHome.this,
+                    "onScanFailed: " + String.valueOf(errorCode),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        private void addBluetoothDevice(BluetoothDevice device){
+            if(!listBluetoothDevice.contains(device)){
+                listBluetoothDevice.add(device);
+                listView.invalidateViews();
+            }
+        }
+    };
 }
